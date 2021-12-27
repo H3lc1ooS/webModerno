b7ddebf8386d81e43cf6d4abc20c4bfaaca88bd1
H3lc1ooS <helciosilvagames@gmail.com>
2021-12-27 14:22:40 +0100

    Constante do PI

--- javaScript/udemy/inicio/exercises/ex02_area.js
+++ javaScript/udemy/inicio/exercises/ex02_area.js
@@ -3,6 +3,6 @@
 // Pi * raio * raio = area da circunferência
 
 let raio = 10;
-let pi = 3.14;
+const PI = 3.14;
 let area = pi * (raio*raio);
 console.log ('A área da circunferência é de ' + area + "m");
