58cc3feb737fcac95414474a5490f34e27c6276b
H3lc1ooS <helciosilvagames@gmail.com>
2021-12-27 14:20:14 +0100

    Exercício da Área

---
+++ javaScript/udemy/inicio/exercises/ex02_area.js
@@ -0,0 +1,8 @@
+//Calcule a área da circunferência
+
+// Pi * raio * raio = area da circunferência
+
+let raio = 10;
+let pi = 3.14;
+let area = pi * (raio*raio);
+console.log ('A área da circunferência é de ' + area + "m");
